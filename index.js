92f9097f466330f989f0b1a983d73a65799ff7e3
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-12-02 15:13:18 -0300

    basic organization

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
+// DEPENDENCIES
 const express = require('express');
+const bodyParser = require('body-parser');
+// const rescue = require('express-rescue');
+require('dotenv').config();
 
+// IMPORTATIONS
 const app = express();
 
-// não remova esse endpoint, e para o avaliador funcionar
+app.use(bodyParser.json());
+
+// const usersController = require('./controllers/usersController');
+// const recipesController = require('./controllers/recipesController');
+
+// ENDPOINTS
+
+// 0 - Não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+// app.use('/path', users||recipesController);
+
+// Usar middleware static para permitir acesso a imagens via a API
+// const path = require('path');
+// ...
+// /images é o caminho da API onde as imagens estarão disponíveis
+// path.join(__dirname, 'uploads') é o caminho da pasta onde o multer salva suas imagens ao realizar o upload
+// app.use('/images', express.static(path.join(__dirname, 'uploads')));
+// ...
+
+// PORT LISTENER
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Yummy, here is ${PORT} port`));
---
+++ models/connection.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
+
+const { MongoClient } = require('mongodb');
+
+const connection = () =>
+  MongoClient.connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+
+module.exports = connection;
