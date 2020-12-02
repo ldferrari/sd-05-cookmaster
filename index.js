72cdf3593adcbef1ef48b3de39d6f4780399b30a
binhus <eng.robsonwilli@gmail.com>
2020-12-02 17:37:21 -0300

    começando o projeto

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const parser = require('body-parser');
+
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(parser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
+});
+
+app.listen(PORT, () => {
+  console.log(`O PAI TÁ ON NA PORTA ${PORT}`);
 });
