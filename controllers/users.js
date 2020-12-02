a8b5352b80e1a3d64f4faab2514e1125f7ecf1d7
Felipe Vieira <flpvieira86@gmail.com>
2020-12-02 20:01:41 -0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const rescue = require('express-rescue');
-const { add } = require('../models');
+const { addUser } = require('../models');
 const { validateUser } = require('../middlewares/index');
 
 const usersController = express.Router();
 
 usersController.post('/', validateUser, rescue(async (req, res) => {
   const { name, email, password } = req.body;
-  const user = await add({ name, email, password });
+  const user = await addUser({ name, email, password });
 
   res.status(201).json({ user });
 }));
