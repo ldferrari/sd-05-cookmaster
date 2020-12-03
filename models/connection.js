50e88125f1de74287996a35a048ccb7853955f28
Felipe Vieira <flpvieira86@gmail.com>
2020-12-02 22:57:27 -0300

    change db url

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-// const DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-const DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// const DB_URL = 'mongodb://localhost:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 
 let connection;
