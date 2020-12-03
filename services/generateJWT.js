d72c488fc417f8faa23c09a996ceb5ef135ae2d9
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-12-03 19:40:07 -0300

    eslint generateJWT.js

--- services/generateJWT.js
+++ services/generateJWT.js
@@ -2,23 +2,23 @@ const jwt = require('jsonwebtoken');
 
 const generateJWT = async (user) => {
   // 1/ Secret
-   const secret = 'secret-stuff-here-what?';
-   // 2/ Header
-   const jwtConfig = {
-     expiresIn: '1d',
-     algorithm: 'HS256',
-   };
-   // 3/ Payload
-   // First taking away sensible data
-   const { password: _, ...userWithoutPassword } = user;
-   const { _id: id } = userWithoutPassword;
-   const payload = {
-     sub: id,
-     userData: userWithoutPassword,
-   };
-   // 4/ Assinatura
-   const token = jwt.sign(payload, secret, jwtConfig);
-   return token;
- }
+  const secret = 'secret-stuff-here-what?';
+  // 2/ Header
+  const jwtConfig = {
+    expiresIn: '1d',
+    algorithm: 'HS256',
+  };
+  // 3/ Payload
+  // First taking away sensible data
+  const { password: _, ...userWithoutPassword } = user;
+  const { _id: id } = userWithoutPassword;
+  const payload = {
+    sub: id,
+    userData: userWithoutPassword,
+  };
+  // 4/ Assinatura
+  const token = jwt.sign(payload, secret, jwtConfig);
+  return token;
+};
 
- module.exports = generateJWT;
+module.exports = generateJWT;
