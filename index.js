05f1ba8e3a81453779d4157c5e2e1406d9fbf699
Felipe Vieira <flpvieira86@gmail.com>
2020-12-02 23:06:46 -0300

    tentativa 4

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const { usersController, loginController, recipesController } = require('./controllers/index');
 
@@ -8,14 +9,14 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-app.use('/images', express.static('uploads'));
-
-app.get('/', (_, res) => { res.send(); });
+app.use('/images', express.static(path.join(__dirname, '/uploads')));
 
 app.use('/users', usersController);
 
 app.use('/login', loginController);
 
 app.use('/recipes', recipesController);
+
+app.get('/', (_, res) => { res.send(); });
 
 app.listen(port, () => console.log(`port: ${port}`));
