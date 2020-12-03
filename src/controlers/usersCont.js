d3eea37c02a4121cb9bad79945de625ec8d94e9d
Rafael Assad <rafaassadeng@gmail.com>
2020-12-03 15:31:17 -0300

    ta osso

--- src/controlers/usersCont.js
+++ src/controlers/usersCont.js
@@ -6,16 +6,16 @@ const service = require('../services/usersServices');
 const route = Router();
 
 route.get('/', rescue(async (req, res) => {
-    const allUsers = await service.getAll();
-    res.status(200).json({ allUsers });
-  }));
+  const allUsers = await service.getAll();
+  res.status(200).json({ allUsers });
+}));
 
 route.post('/', rescue(async (req, res, next) => {
-    const user = await service.create(req.body);
-    if (user.error) {
-      return next(user);
-    }
-    res.status(201).json({ user });
-  }));
+  const user = await service.create(req.body);
+  if (user.error) {
+    return next(user);
+  }
+  res.status(201).json({ user });
+}));
 
 module.exports = route;
