8a9ae38e7d4bc7b1cfb32a8f66708ca95e74f9fc
hleoc <informig@gmail.com>
2020-12-04 10:49:44 -0300

    Iniciando o projeto - #VQV

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
+});
+// ------------------------------------
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`O pai tá ON no projeto e na porta ${PORT}`);
 });
