11783b34ba6f78ba0e72ea77b107369705a26223
César Alves <alvescesar@hotmail.com>
2020-12-04 18:27:00 -0300

    'It's RAAAAAAAAAAAWWWgit add .'

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const app = express();
 
-// não remova esse endpoint, e para o avaliador funcionar
+// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
