374a9158fa41e47eb50771e8f89d9c09f4131a96
Anderson Godoy <anderson.a.godoy@gmail.com>
2020-12-05 20:18:24 -0300

    alterado banco para gitub reconhecer teste

--- models/get-collection.js
+++ models/get-collection.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: 'variables.env' });
 
 const DB_NAME = 'Cookmaster';
 // SOMENTE PARA DESENVOLVIMENTO LOCAL
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const MONGO_DB_URL = process.env.LOCAL_SERVER;
 // SOMENTE PARA TESTE REMOTO NO GITHUB
 // const MONGO_DB_URL = process.env.REMOTE_SERVER;
 
