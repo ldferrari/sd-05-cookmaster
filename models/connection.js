4fbae34f4e1331260053a62faef88b3b2c8b5917
Preto115 <preto115@gmail.com>
2020-12-06 10:30:06 -0300

    Elisangelo_iniciando projeto-Cockmaster

---
+++ models/connection.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require('mongodb');
+
+// não esquecer de mudar a URL para o avaliador funcionar
+
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/CockMaster';
+
+// const MONGO_DB_URL = 'mongodb: //localhost:27017/Cockmaster';
+const MONGO_DB_URL = 'mongodb: //mongo:27017/Cockmaster';
+
+const DB_NAME = 'Cockmaster';
+
+const connection = (collectionName) => MongoClient
+  .connect(MONGO_DB_URL, {
+    useNewUrlParse: true,
+    useUnifiedTopology: true })
+  .then((connect) => connect.db(DB_NAME).collection(collectionName))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
+module.exports = connection;
