d74afbb470922d5b8f3b689dfd1b3a452cb5ea19
ferrari <luisdiasferrari@gmail.com>
2020-12-07 19:24:25 -0300

    requisito 01

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,29 +1,38 @@
 const { Router } = require('express');
-// const usersService = require('../service/usersService');
-const userBD = require('../models/usersModels');
-// const rescue = require('express-rescue');
+const jwt = require('jsonwebtoken');
+const userModels = require('../models/usersModels');
 
 const loginRouter = Router();
+const secret = 'é segredo';
+const jwtConfig = {
+  expiresIn: '15m', // tempo para o token inspirar
+  algorithm: 'HS256', // algorítimo assimétrico (algotítimo padrão, não necessário colocar)
+};
 
-/* const crypto = require('crypto-js'); // Gerador de tokens
-const { MD5 } = crypto;
-const token = MD5().toString().substr(0, 16);
- */
-loginRouter.get('/login', async (req, res) => {
+loginRouter.post('/', async (req, res) => {
   try {
     const { email, password } = req.body;
-    const userFound = await userBD.findByEmail(email);
+    const userFound = await userModels.findByEmail(email);
     console.log(email);
 
     if (!email || !password) {
       return res.status(401).json({ message: 'All fields must be filled' });
     }
-
     if (!userFound || userFound.password !== password) {
-      return res.status(401).json({ message: 'All fields must be filled' });
+      return res.status(401).json({ message: 'Incorrect username or password' });
     }
-    return res.status(200).json('token');
-  } catch {
+
+    const payload = {
+      iss: 'post-api', // issuer -> quem emitiu o token
+      aud: 'post-api', // Audience -> quem deve aceitar o token
+      sub: userFound.id, // Suject -> A quem pertence esse token
+      useData: userFound,
+    };
+    const token = jwt.sign(payload, secret, jwtConfig);
+
+    return res.status(200).json({ token });
+  } catch (err) {
+    console.log(err.message);
     return res.status(500).json({ messade: 'Algo deu errado no login' });
   }
 });
