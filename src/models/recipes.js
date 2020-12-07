bdde08df2b6d4752023d1e68786e3f0646a86c0b
Rafael Assad <rafaassadeng@gmail.com>
2020-12-07 11:14:24 -0300

    5 lint

--- src/models/recipes.js
+++ src/models/recipes.js
@@ -5,9 +5,7 @@ const getAll = async () =>
   getCollection('recipes').then((collection) => collection.find().toArray());
 
 const create = async ({ name, ingredients, preparation, userId }) => {
-  const recipe = await getCollection('recipes').then((collection) =>
-    collection.insertOne({ name, ingredients, preparation, userId })
-  );
+  const recipe = await getCollection('recipes').then((collection) => collection.insertOne({ name, ingredients, preparation, userId }));
   return { name, ingredients, preparation, userId, _id: recipe.insertedId };
 };
 
