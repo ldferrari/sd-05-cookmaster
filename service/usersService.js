40d4b751d376b26743d6e1459c01b16fd64da8e7
Virginia Duca <virginiambd.bio@gmail.com>
2020-12-07 11:11:27 -0300

    eslint

--- service/usersService.js
+++ service/usersService.js
@@ -2,26 +2,22 @@ const model = require('../model/usersModel');
 
 const createUser = async (name, email, password) => {
   if (!name || !email || !password || !email.match(/\S+@\S+\.\S+/)) {
-    // eslint-disable-next-line no-throw-literal
-    throw { code: 400, message: 'Invalid entries. Try again.' };
+    throw new Error({ code: 400, message: 'Invalid entries. Try again.' });
   }
   const emailExists = await model.getByEmail(email);
   if (emailExists) {
-    // eslint-disable-next-line no-throw-literal
-    throw { code: 409, message: 'Email already registered' };
+    throw new Error({ code: 409, message: 'Email already registered' });
   }
   return model.createUser(name, email, password);
 };
 
 const login = async (email, password1) => {
   if (!email || !password1) {
-    // eslint-disable-next-line no-throw-literal
-    throw { code: 401, message: 'All fields must be filled' };
+    throw new Error({ code: 401, message: 'All fields must be filled' });
   }
   const emailExists = await model.getByEmail(email);
   if (!emailExists || password1 !== emailExists.password) {
-    // eslint-disable-next-line no-throw-literal
-    throw { code: 401, message: 'Incorrect username or password' };
+    throw new Error({ code: 401, message: 'Incorrect username or password' });
   }
   const { password: _, ...userData } = emailExists;
   console.log(userData);
