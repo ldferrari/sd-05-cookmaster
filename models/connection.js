9903fd45301207ead4a032ca9390f576f00b811c
Preto115 <preto115@gmail.com>
2020-12-06 22:50:19 -0300

    rtentando soluiconar erro no processo de validacao no mongo

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,11 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 // não esquecer de mudar a URL para o avaliador funcionar
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
 
 // const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const MONGO_DB_URL = 'mongodb://mongo:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://mongo:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 
