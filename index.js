45960a61733b48f2a3d933482e0613684c5510c6
samueap <samueap@gmail.com>
2020-12-09 17:02:44 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
+});
+
+app.use(bodyParser.json());
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Escutando porta ${PORT}, Xablau`);
 });
