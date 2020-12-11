60b83f73f552c1775d3580fc8656fd51b04e2077
Elisângelo <64562701+preto115@users.noreply.github.com>
2020-12-10 21:01:16 -0300

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 
-require('dotenv').config();
 
 // const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 
