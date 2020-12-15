bf7212a43bed2b643717ca0e1168918b8010b37f
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-12-15 15:20:29 -0300

    Create enumerators to handle magic numbars/strings

---
+++ enumerators/databaseEnums.js
@@ -0,0 +1,8 @@
+const TABLES = {
+  users: 'users',
+  recipes: 'recipes',
+};
+
+module.exports = {
+  TABLES,
+};
---
+++ enumerators/usersEnums.js
@@ -0,0 +1,16 @@
+const ROLES = {
+  user: 'user',
+};
+
+const error = {
+  invalidEntries: {
+    message: 'Invalid entries. Try again.',
+  },
+  emailExists: {
+    message: 'Email already registered',
+  },
+};
+
+module.exports = {
+  ROLES, error,
+};
