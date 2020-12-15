989e9c5484e6fedc19749955d90aa60c890c3891
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-12-15 15:54:09 -0300

    Requisito 1: Atende as especificaçoes.

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? './.env.testing' : './.envKyle',
+});
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const usersRoutes = require('./routes/users.routes');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.use('/users', usersRoutes);
+
+app.get('/ping', (_req, res, _next) => { res.send('pong'); });
 
 // não remova esse endpoint, e para o avaliador funcionar
+// Ok Jean, não vou retirar esse endpoint
 app.get('/', (request, response) => {
   response.send();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Eis que estou a ${PORT} e bato.`);
 });
