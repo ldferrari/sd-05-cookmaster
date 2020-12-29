bd7e96b06458060d6aaf45d3ed2f744eade4b14b
pauloricardoz <trybe.przt@gmail.com>
2020-12-29 16:55:18 -0300

    seed correction

--- seed.js
+++ seed.js
@@ -1,6 +1,1 @@
-// colocar query do MongoDB
-const connection = require('./models/connection');
-
-connection('users').then((db) =>
-  db.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' })
-);
+db.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
