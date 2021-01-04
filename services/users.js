77fb0f2c0fcf3bcb34097b9c9a959951f5b50d0b
marinabrcls <barcelos.marina@gmail.com>
2021-01-04 18:58:03 -0300

    users service fix

--- services/users.js
+++ services/users.js
@@ -1,27 +1,28 @@
-const { findEmail, createUser } = require('../models/users');
+const { findEmail } = require('../models/users');
 
-const verifyNewUser = async (name, email, password) => {
+const verifyNewUser = async (req, res, next) => {
+  const { name, email, password } = req.body;
   const regex = /^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
 
   // [Será validado que o campo "name", "email" e "senha" são obrigatórios]
   // [Será validado que não é possível cadastrar usuário com o campo email inválido]
   if (!name || !password || !email || !regex.test(email)) {
-    return {
+    return res.status(400).json({
       code: 'invalidEntries',
       message: 'Invalid entries. Try again.',
-    };
+    });
   }
 
   // [Será validado que o campo "email" é único]
   const existentUser = await findEmail(email);
   if (existentUser) {
-    return {
+    return res.status(409).json({
       code: 'emailExists',
       message: 'Email already registered',
-    };
+    });
   }
 
-  return createUser(name, password, email);
+  next();
 };
 
 module.exports = {
