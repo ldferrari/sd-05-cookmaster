1adc2f0bd2224adac6c4ec54da2b7c0272acfa92
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-01-14 23:53:11 -0300

    novamente ajustes

--- controllers/recipesControllers.js
+++ controllers/recipesControllers.js
@@ -4,39 +4,64 @@ const jwt = require('jsonwebtoken');
 const rm = require('../models/recipesModel');
 const um = require('../models/usersModel');
 
-const { recipesValidation } = require('../services/recipeValidation');
+const { recipesValidation, deleteRecipeValidation } = require('../services/recipeValidation');
 
 const appRouter = express.Router();
 
 appRouter.post('/recipes', recipesValidation, rescue(async (req, res, _next) => {
-  const { authorization } = req.headers;
-  const { name, ingredients, preparation } = req.body;
-  const decode = await jwt.verify(authorization, 'batata');
-  const user = await um.findByEmail(decode.data.email);
-  const recipe = await rm.createRecipe(name, ingredients, preparation, user.id);
-  return res.status(201).json({ recipe });
+  try {
+    const { authorization } = req.headers;
+    const { name, ingredients, preparation } = req.body;
+    const decode = await jwt.verify(authorization, 'batata');
+    const user = await um.findByEmail(decode.data.email);
+    const recipe = await rm.createRecipe(name, ingredients, preparation, user.id);
+    return res.status(201).json({ recipe });
+  } catch (err) {
+    console.log(err.message);
+  }
 }));
 
-appRouter.get('/recipes', rescue(async (req, res, _next) => {
-  const allRecipes = await rm.getAllRecipes();
-  res.status(200).send(allRecipes);
+appRouter.get('/recipes', rescue(async (_req, res, _next) => {
+  try {
+    const allRecipes = await rm.getAllRecipes();
+    return res.status(200).send(allRecipes);
+  } catch (err) {
+    console.log(err.message);
+  }
 }));
 module.exports = appRouter;
 
 appRouter.get('/recipes/:id', rescue(async (req, res, _next) => {
-  const { id } = req.params;
-  const recipeId = await rm.recipeByIdSearch(id);
-  console.log(recipeId);
-  if (recipeId === null) {
-    return res.status(404).json({ message: 'recipe not found' });
+  try {
+    const { id } = req.params;
+    const recipeId = await rm.recipeByIdSearch(id);
+    if (recipeId === null) {
+      return res.status(404).json({ message: 'recipe not found' });
+    }
+    return res.status(200).json(recipeId);
+  } catch (err) {
+    console.log(err.message);
   }
-  res.status(200).json(recipeId);
 }));
 
 appRouter.put('/recipes/:id', recipesValidation, rescue(async (req, res, _next) => {
-  const { id } = req.params;
-  const { name, ingredients, preparation } = req.body;
-  await rm.editRecipe(id, name, ingredients, preparation);
-  const upRecipe = await rm.recipeByIdSearch(id);
-  res.status(200).json(upRecipe);
+  try {
+    const { id } = req.params;
+    const { name, ingredients, preparation } = req.body;
+    await rm.editRecipe(id, name, ingredients, preparation);
+    const upRecipe = await rm.recipeByIdSearch(id);
+    return res.status(200).json(upRecipe);
+  } catch (err) {
+    console.log(err.message);
+  }
+}));
+
+appRouter.delete('/recipes/:id', deleteRecipeValidation, rescue(async (req, res, _next) => {
+  try {
+    const { id } = req.params;
+    rm.removeRecipe(id);
+    return res.status(204).send();
+  } catch (err) {
+    return res.send(500).json(err.message);
+  }
 }));
--- models/recipesModel.js
+++ models/recipesModel.js
@@ -1,7 +1,7 @@
 const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
-const createRecipe = (name, ingredients, preparation, userId) =>
+const createRecipe = async (name, ingredients, preparation, userId) =>
   connection('recipes').then((recipes) =>
     recipes.insertOne({ name, ingredients, preparation, userId }).then((result) => ({
       _id: result.insertedId,
@@ -11,14 +11,18 @@ const createRecipe = (name, ingredients, preparation, userId) =>
       userId,
     })));
 
-const getAllRecipes = () =>
+const getAllRecipes = async () =>
   connection('recipes').then((recipes) => recipes.find({}).toArray());
 
-const recipeByIdSearch = (id) =>
+const recipeByIdSearch = async (id) =>
   connection('recipes').then((recipes) => (ObjectId.isValid(id) ? recipes.findOne({ _id: ObjectId(id) }) : null));
 
-const editRecipe = (id, name, ingredients, preparation) =>
+const editRecipe = async (id, name, ingredients, preparation) =>
   connection('recipes').then((recipes) => (ObjectId.isValid(id) ? recipes.updateOne({ _id: ObjectId(id) }, { $set: { name, ingredients, preparation } })
     : null));
 
-module.exports = { createRecipe, getAllRecipes, recipeByIdSearch, editRecipe };
+const removeRecipe = async (id) => {
+  connection('recipes').then((recipes) => (ObjectId.isValid(id) ? recipes.deleteOne({ _id: ObjectId(id) }) : null));
+};
+
+module.exports = { createRecipe, getAllRecipes, recipeByIdSearch, editRecipe, removeRecipe };
