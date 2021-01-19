8d9ec48a13294abb1b5ba6f5a5426410f5e04685
marinabrcls <barcelos.marina@gmail.com>
2021-01-19 18:15:57 -0300

    bônus

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const rescue = require('express-rescue');
-const { verifyNewUser } = require('../services/users');
+const { verifyToken } = require('../middlewares/verifyToken');
+const { verifyNewUser, verifyNewAdmin } = require('../services/users');
 const { createUser } = require('../models/users');
 
 const usersController = express.Router();
 
+// 11 - Bônus - Cadastramento de admin
+usersController.post('/admin', verifyToken, verifyNewAdmin, rescue(async (req, res) => {
+  const { name, email, password } = req.body;
+  const adminInfos = { name, email, password };
+  const newAdmin = await createUser(adminInfos, 'admin');
+
+  return res.status(201).json({ user: newAdmin });
+}));
+
 // 1 - Crie um endpoint para o cadastro de usuários
 usersController.post('/', verifyNewUser, rescue(async (req, res) => {
   const { name, email, password } = req.body;
-  const newUser = await createUser({ name, email, password });
+  const userInfos = { name, email, password };
+  const newUser = await createUser(userInfos, 'user');
 
   return res.status(201).json({ user: newUser });
 }));
