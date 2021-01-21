0d77ac38d846a09aa8cb693ffe3c76ea1f4cd864
ederusena <eder.sena@live.com>
2021-01-21 19:19:05 -0300

    Requirement 1-Object

--- models/connection.js
+++ models/connection.js
@@ -2,8 +2,8 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const DB_NAME = 'Cookmaster';
-const MONGO_DB_URL = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.jy0rn.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-// const MONGO_DB_URL = process.env.MONGO_DB_URL || `mongodb://mongodb:27017/${DB_NAME}`;
+// const MONGO_DB_URL = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.jy0rn.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_DB_URL = process.env.MONGO_DB_URL || `mongodb://mongodb:27017/${DB_NAME}`;
 
 let connection = null;
 
